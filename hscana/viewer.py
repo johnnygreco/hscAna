bcf4702c1662b7220b91a4acc11a08407139f009
Johnny Greco <jgreco@astro.princeton.edu>
2016-05-24 13:39:31 -0400

    added ellipse color option to viewer.view

--- hscana/viewer.py
+++ hscana/viewer.py
@@ -8,7 +8,7 @@ DATA_DIR = "/tigress/HSC/HSC/rerun/production-20151224/"
 butler = lsst.daf.persistence.Butler(DATA_DIR)
 
 def view(cat=None, exp=None, tract=None, patch=None, ID=None, coords=None, filter='I', frame=0, scale="zscale",\
-         zoom="to fit", trans=80, draw_ells=True, maxsep=None, shape_model='shape.hsm.moments'):
+         zoom="to fit", trans=80, draw_ells=True, maxsep=None, shape_model='shape.hsm.moments', pcolor=ds9.GREEN, ccolor=ds9.RED):
 
     if cat is None:
         assert (tract is not None) and (patch is not None), 'if no cat is given, must give tract and patch'
@@ -33,12 +33,12 @@ def view(cat=None, exp=None, tract=None, patch=None, ID=None, coords=None, filte
         ra, dec = cat.get('coord.ra')*180./np.pi, cat.get('coord.dec')*180./np.pi
         seps = utils.angsep(ra0, dec0, ra, dec)
         cut = seps < maxsep
-        cat = cat[seps < maxsep]
+        cat = cat[seps < maxsep].copy(deep=True)
 
     if draw_ells:
         with ds9.Buffering(): 
             for i,source in enumerate(cat):
-                ellcolor = ds9.GREEN if source.get('parent')==0 else ds9.BLUE
+                ellcolor = pcolor if source.get('parent')==0 else ccolor
                 ixx, ixy, iyy = [source.get(shape_model+_x) for _x in ['.xx', '.xy', '.yy']]
                 symbol = "@:{ixx},{ixy},{iyy}".format(ixx=ixx, ixy=ixy, iyy=iyy)
                 ds9.dot(symbol, source.getX()-x0, source.getY()-y0, ctype=ellcolor, frame=frame)
