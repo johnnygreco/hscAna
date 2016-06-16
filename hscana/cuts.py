65c7ca54643563bac546422a560e06c9baab7223
Johnny Greco <jgreco@astro.princeton.edu>
2016-06-16 17:33:20 -0400

    changed search to a module, now run with script

--- hscana/cuts.py
+++ hscana/cuts.py
@@ -1,4 +1,10 @@
-import numpy as np
+
+"""
+Cuts for UDG search. The cuts are made within the MyCat class. 
+
+cat_cuts --> catalog cuts 
+phy_cuts --> physical cuts (based on parameter measurements)
+"""
 
 cat_cuts = {'flags.pixel.bad':0,
             'flags.pixel.edge':0,
@@ -13,41 +19,3 @@ phy_cuts = {'size_min':1.5, # kpc
             'SB_min':24.0, 
             'SB_max':30.0,
             'absmag_max':-13.0}
-
-def badpix_cut(cat):
-    cut  = cat['flags.pixel.bad'] == 0
-    cut &= cat['flags.pixel.edge'] == 0
-    cut &= cat['flags.pixel.interpolated.any'] == 0
-    cut &= cat['flags.pixel.cr.any'] == 0
-    cut &= cat['flags.pixel.saturated.any'] == 0
-    return cut
-
-def source_cut(cat):
-    cut  = cat['classification.extendedness'] > 0.5
-    cut &= cat['parent'] == 0 
-    cut &= cat['flags.pixel.bright.object.any'] == 0
-    return cut
-
-def basic_cut(cat):
-    cut  = badpix_cut(cat) 
-    cut &= source_cut(cat)
-    return cut
-
-def size_cut(angsize, D_A, minsize=1.0):
-    """
-    input parameters
-    ----------------
-    angsize: ndarray, angular sizes in arcsec
-    D_A: float, the angular diameter distance to the group in Mpc
-
-    optional input
-    --------------
-    minsize: float, minumum size for cut in kpc
-    """
-    size = angsize*D_A*(1.0/206265.)*1.0e3 # size in kpc
-    cut = angsize > minsize
-    return cut
-
-def SB_cut(SB, SBmin=24., SBmax=30.0):
-    cut = (SB>SBmin) & (SB<SBmax)
-    return cut
--- run_search.py
+++ run_search.py
@@ -1,105 +1,36 @@
 #!/usr/bin/env python 
 
-import sys
-import hscana
-import numpy as np 
-import argparse
-from toolbox.astro import angsep
+import numpy as np
 from astropy.table import Table
-from hscana.utils import get_hsc_regions, skybox
+import hscana
+from hscana.search import group_search
 group_info = Table.read('/home/jgreco/data/groups/group_info.csv')
+print len(group_info), 'galaxy groups in catalog'
 
-def run(group_id, band='I', box_width=3.0, max_sep=2.0, butler=None):
-    """
-    Run script to search for UDG candidates.
+gemini_cuts = True
 
-    Parameters
-    ----------
-    group_id : int
-        Galaxy group identification number.
-    band : string, optional, default is 'I'
-        HSC band (G, R, I, Z, or Y).
-    box_width : float, optional, default is 3 degree
-        Angular width of 'skybox' to extract around the groups
-        luminosity weighted center. Must be in degrees.
-    max_sep : float, optional, default is 2 arcsec
-        Maximum separation for which two objects are considered
-        to be the same object. Must be in arcseconds. 
-    butler : Butler object, optional
-        If None, will be created within function. If you are
-        looping over many groups, you should create a butler
-        once outside this function. 
-    """
-    if butler is None:
-        butler = hscana.get_butler()
-    idx = np.argwhere(group_info['group_id']==group_id)[0,0]
-    ra_c, dec_c, D_A, D_L = group_info['ra', 'dec', 'D_A', 'D_L'][idx]
-    theta = (box_width/D_A)*180.0/np.pi
-    group_regions = get_hsc_regions(skybox(ra_c, dec_c, theta, theta))
-    print 'We will extract region of angular size theta =', round(theta, 3), 'degree'
+##########################################################
+# Make cuts on group catalog
+##########################################################
+if gemini_cuts:
+    zmax = 0.08
+    Ngal_max = 15
+    # only look at ra's accessible to gemini
+    cut  = (group_info['ra'] > 12.5*15) | (group_info['ra'] < 5.0*15)
+    cut &= group_info['z'] <= zmax
+    cut &= group_info['Ngal'] <= Ngal_max
+    group_info = group_info[cut]
+    print len(group_info), 'galaxy groups after cuts'
 
-    # build list of udg candidates
-    candy = []
-    for tract, patch in group_regions:
-        # some tracts and patches are missing
-        print tract, patch
-        try:
-            mycat = hscana.MyCat(tract, patch, band, group_id, makecuts=True, butler=butler)
-        except:
-            print '!!!!! FAILED !!!!!'
-            continue
-        if mycat.count()>0:
-            candy.append(mycat)
-            
-    # get coordinates of all candidates
-    coords = []
-    for i in range(len(candy)):
-        for ra, dec in candy[i].coord():
-            coords.append((ra, dec))
-    coords = np.array(coords)
-            
-    # build mask for double entries
-    mask = np.ones(coords.shape[0], dtype=bool)
-    for i, (ra, dec) in enumerate(coords):
-        if mask[i]==True:
-            unique = angsep(ra, dec, coords[:,0], coords[:,1]) > max_sep
-            unique[i] = True
-            mask &= unique
-    coords = coords[mask]
-    print 'number of candidates =', coords.shape[0]
-
-    # output in format for hscMap
-    if coords.shape[0]>0:
-        np.savetxt('output/group_'+str(group_id)+'_coords.csv', coords, delimiter=',', header='ra,dec')
-    else:
-        print 'group', group_id, 'has zero candidates'
-
-if __name__=='__main__':
-    # for usage, enter ./run_search.py
-    parser = argparse.ArgumentParser(description='Search for UDG candidates.')
-    parser.add_argument('-g', '--group_id', type=int, default=None, help='run search for single group with id=group_id')
-    parser.add_argument('-n', '--Ngal', type=int, default=None, help='run search on all groups with <= Ngal galaxies')
-    parser.add_argument('-z', '--z', type=float, default=None, help='run search on all groups with redshift < z')
-    args = parser.parse_args()
-    if args.group_id is not None:
-        print 'running search for group', args.group_id
-        run(group_id=args.group_id)
-    elif args.Ngal is not None:
-        butler = hscana.get_butler()
-        cut = group_info['Ngal']<=args.Ngal
-        print 'running search for all groups with Ngal <=', args.Ngal
-        if args.z is not None:
-            cut &= group_info['z']<args.z
-            print 'and z <', args.z
-        for ID in group_info[cut]['group_id']:
-            print '***** searching in group '+str(ID)+' *****'
-            run(group_id=ID, butler=butler)
-    elif args.z is not None:
-        butler = hscana.get_butler()
-        cut = group_info['z']<args.z
-        print 'running search for all groups with z <', args.z
-        for ID in group_info[cut]['group_id']:
-            print '***** searching in group '+str(ID)+' *****'
-            run(group_id=ID, butler=butler)
-    else:
-        parser.print_help()
+##########################################################
+# Perform UDG search within all remaining groups
+##########################################################
+butler = hscana.get_butler()
+for ID in group_info['group_id']:
+    print '***** searching in group '+str(ID)+' *****'
+    idx = np.argwhere(group_info['group_id']==ID)[0,0]
+    ra, dec, z, Ngal = group_info['ra', 'dec', 'z', 'Ngal'][idx]
+    print 'ra dec =', ra, dec
+    print 'z =', z
+    print 'Ngal=', Ngal 
+    group_search(group_id=ID, butler=butler)
